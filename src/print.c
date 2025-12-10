cbc81ff04c91c50307baf768a302cb34a94d4a4d
Amauri <dsrsoft2010@hotmail.com>
2025-12-10 11:33:44 -0400

    feat(print()): Print a list of integer values

--- src/print.c
+++ src/print.c
@@ -1,4 +1,30 @@
-/* print.c -- prints an array */
+/*
+* =====================================
+* Module: print()
+* =====================================
+*
+* =====================================
+* Description:
+* =====================================
+* Print a list of integer values
+* =====================================
+*
+* =====================================
+* Inputs:
+* =====================================
+*   @array - Integer array
+*   @limit - Number of elements in @array
+*
+* =====================================
+* Process:
+* =====================================
+*   1. Declare index variable
+*   2. Go throug from @index to @limit
+*   3. Print a value on @array[@index]
+*   4. Evaluate if @index % is equal 9 or 10 elements to print a new line
+*   5. To loop end and @index is no % 0 print a new line
+*
+*/
 #include <stdio.h>
 void print(const int array[], int limit)
 {
